956489091575881c939a30a5474e30ff8d96d4ad
Vishaka Basnayake <vishakabasnayake@gmail.com>
2022-05-14 09:25:37 +0530

    update

--- scramblingcode_d2d_noma/samplecodes/fractional_optimization_v2.m
+++ scramblingcode_d2d_noma/samplecodes/fractional_optimization_v2.m
@@ -98,6 +98,7 @@ s4   = 0.1;
 
 %vectors
 interf_vec  = zeros(K,1);
+sumsym_dur_vec = zeros(K,1);
 sym_dur_vec = [s1;s2;s3;s4];
 power_vec   = [p1;p2;p3;p4];
 g_vec       = [g1;g2;g3;g4];%channel gain vector
@@ -105,9 +106,10 @@ g_th        = communication_radius.^g_th;
 rate_th     = log2( 1 + max_tx_power*g_th/noisepower);
 desired_id  = 1;
 
-for k =1:K
+for k =1:K%interference
     if k ~= desired_id
         interf_vec(k) = power_vec(k)*g_vec(k)*sym_dur_vec(k) + interf_vec(k);
+        sumsym_dur_vec(k)= sym_dur_vec(k) + sumsym_dur_vec(k);
     end   
 end
 
@@ -115,24 +117,89 @@ grad_uk = zeros(K,1);
 uk      = zeros(K,1);
 
 uk(:,n) = ones(K,1);
+lam1k(:,n) = ones(K,1);
+lam2k(:,n) = ones(K,1);
+lam3k(:,n) = ones(K,1);
+lam4k(:,n) = ones(K,1);
+lam5k(:,n) = ones(K,1);
+
+grad_lam1k = zeros(K,1);
+grad_lam2k = zeros(K,1);
+grad_lam3k = zeros(K,1);
+grad_lam4k = zeros(K,1);
+grad_lam5k = zeros(K,1);
+
+%stop criterian
+euk=2;%uk
+elam1k=10;
+elam2k=100;
+elam3k=100;
+elam4k=100;
+elam5k=100;
 
 %% iterations
 %consider the grad of the objective function 
+%check for different snr values
 for iter =1:nbiter
     n =iter;
-    disp(n)
+    
     %define u vector and give an initial value to it
-    uk(:,n+1)       = uk(:,n) + t*grad_uk(:,n);
-    grad_uk(:,n+1)= K^2 - lam1*K - lam2*K - 2*lam2*uk(n)*K ...
-              -lam3*mean(g_th)*K - lam3*mean(g_vec,2) - ...
-              lam4*max_tx_power*mean(g_th,2)*(noisepower^2+(interf_vec))...
-              - lam5 - lam5;
-    %u(:,n+1)  = u(:,n+1) >0;%convert to binary
-    if(n >1 & uk(:,n+1)==uk(:,n))
+    uk(:,n+1)      = uk(:,n) + t*grad_uk(:,n);
+    
+    %u(:,n+1) = u(:,n+1) >0;%convert to binary
+    if(n >2 & mean(abs(uk(:,n+1)-uk(:,n)))< euk)%uk converge 
         disp('yes');
+        disp(n);
         disp(uk(:,n+1));
         disp(uk(:,n));
         break;
+    else
+        %check convergence of lamda and if not update using gradient descent
+        lam1k(:,n+1)  = lam1k(:,n) + t*grad_lam1k(:,n);
+        if (n >2 & lam1k(:,n+1)-lam1k(:,n)> elam1k)
+            grad_lam1k(:,n+1) = - sum(uk(:,n)-1);
+        else
+            grad_lam1k(:,n+1) = grad_lam1k(:,n);
+        end   
+        
+        lam2k(:,n+1) = lam2k(:,n) + t*grad_lam2k(:,n);
+        if (n >2 & lam2k(:,n+1)-lam2k(:,n)> elam2k)
+            grad_lam2k(:,n+1)= - sum(uk(:,n)-uk(:,n).^2);
+        else
+            grad_lam2k(:,n+1) = grad_lam2k(:,n);
+        end   
+        
+        lam3k(:,n+1) = lam3k(:,n) + t*grad_lam3k(:,n);
+        if (n >2 & lam3k(:,n+1)-lam3k(:,n)> elam3k)
+            grad_lam3k(:,n+1)= - sum(uk(:,n).*mean(ones(K,1).*g_th - g_vec,2));
+        else
+            grad_lam3k(:,n+1) = grad_lam3k(:,n);
+        end
+        
+        lam4k(:,n+1) = lam4k(:,n) + t*grad_lam4k(:,n);
+        if (n >2 & lam4k(:,n+1)-lam4k(:,n)> elam4k)
+            grad_lam4k(:,n+1)= - sum(uk(:,n).*ones(K,1).*max_tx_power...
+            *mean(g_th,2).*(noisepower^2+(interf_vec)) ...
+            - power_vec.*mean(g_vec,2)*noisepower^2);
+        else
+            grad_lam4k(:,n+1) = grad_lam4k(:,n);
+        end
+          
+        lam5k(:,n+1) = lam5k(:,n) + t*grad_lam5k(:,n);
+        if (n >2 & lam4k(:,n+1)-lam4k(:,n)> elam4k)        
+            grad_lam5k(:,n+1)= - sum(uk(:,n).*sumsym_dur_vec);
+        else
+            grad_lam5k(:,n+1) = grad_lam5k(:,n);
+        end
     end
     
+    grad_uk(:,n+1) = K - lam1k(:,n) - lam2k(:,n) - 2*lam2k(:,n).*uk(:,n) ...
+              -lam3k(:,n).*(mean(g_th)-mean(g_vec,2)) ...
+              -lam4k(:,n).*(max_tx_power*mean(g_th,2)*(noisepower^2+(interf_vec))... 
+              -power_vec.*mean(g_vec,2)*noisepower^2)...
+              - lam5k(:,n).*sumsym_dur_vec;
+          
+     %include kkt consitions as additional constraints to narrow the uk
+     %solution
+     
 end
